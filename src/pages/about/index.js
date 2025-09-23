224a45eb9c64b7468fdd64e562dcf60856bb27e6
Pramod Vishwakarma <pramod00108@gmail.com>
2025-09-23 15:21:35 +0530

    latest_code

---
+++ src/pages/about/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../../components/Header';
+
+const AboutUs = ({ messageApi }) => {
+    return (
+        <div>
+            <Header messageApi={messageApi} />
+        </div>
+    )
+};
+
+export default AboutUs;
